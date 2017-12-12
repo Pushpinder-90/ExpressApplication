a299f75ba8facd8401d77d99d88b343d6b332b4e
pussingh <pussingh@qasource.com>
2017-12-12 17:54:02 +0530

    Initial Commit.

---
+++ serverProcessPost.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+
+
+var server = app.listen(8081 , function(){
+	var host = server.address().host;
+	var port = server.address().port;
+});
+
+var 
+
+app.get('/index.html', function(req, res){
+	console.log("Inside root !!!"+req);
+	res.sendFile(__dirname+"/"+"index.html");
+});
+
+app.get('/process_get', function(req,res){
+	// creating response in JSON form 
+	response = {
+		fname : req.query.first_name,
+		lname : req.query.last_name
+	};
+	console.log("response :: "+response);
+	res.end(JSON.stringify(response))
+});
+
+
