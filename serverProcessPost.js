eae6049162779be9a7d2e6b599283a41c7c1fe34
Pushpinder  Singh <pushpinder_19singh@yahoo.co.in>
2017-12-15 22:31:32 +0530

    Update serverProcessPost.js

--- serverProcessPost.js
+++ serverProcessPost.js
@@ -1,24 +1,24 @@
 const express = require('express');
-var app = express();
-
+const app = express();
+const bodyParser = require('body-parser');
 
 var server = app.listen(8081 , function(){
 	var host = server.address().host;
 	var port = server.address().port;
 });
 
-var 
+const urlEncodedParser = bodyParser.urlencoded({ extended:false});
 
 app.get('/index.html', function(req, res){
 	console.log("Inside root !!!"+req);
 	res.sendFile(__dirname+"/"+"index.html");
 });
 
-app.get('/process_get', function(req,res){
+app.post('/process_post', urlEncodedParser, function (req, res){
 	// creating response in JSON form 
 	response = {
-		fname : req.query.first_name,
-		lname : req.query.last_name
+		fname : req.body.first_name,
+		lname : req.body.last_name
 	};
 	console.log("response :: "+response);
 	res.end(JSON.stringify(response))
